4c6ddd074884c75ac7bc82d26f86c6cb09062d50
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-06-05 20:24:12 +0900

    board & square components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import "./App.css";
+import Board from "./components/Board";
+
 function App() {
   return (
     <div className="game">
-      <div className="game-board">game-board</div>
+      <div className="game-board">
+        <Board />
+      </div>
       <div className="game-info">game-info</div>
     </div>
   );
